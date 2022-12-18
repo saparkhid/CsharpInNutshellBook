9a0797d65e73339254eea1cf1002273bdf30ed39
saparkhid <parkhidpage@gmail.com>
2022-12-18 21:45:55 +0330

    new project

--- Ch3/Ch3_2.LocalMethod/Ch3_2.LocalMethod/Program.cs
+++ Ch3/Ch3_2.LocalMethod/Ch3_2.LocalMethod/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 WriteData("agent 1");
 WriteData("agent 2");
 Console.ReadKey();
--- Ch3/Ch3_2.StaticLocalMethods/Ch3_2.StaticLocalMethods/Program.cs
+++ Ch3/Ch3_2.StaticLocalMethods/Ch3_2.StaticLocalMethods/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+WriteData("agent 1");
+WriteData("agent 2");
+Console.ReadKey();
+void WriteData(string sender)
+{
+    Square(7);
+    Cube(7);
+    static void Square(int v)
+    {
+        int sq = v * v;
+        Console.WriteLine("because static modifier, the sender variable from outer function is not visible.");
+    }
+    void Cube(int v)
+    {
+        int c = v * v * v;
+        Console.WriteLine($"{sender}:c");
+    }
+}
