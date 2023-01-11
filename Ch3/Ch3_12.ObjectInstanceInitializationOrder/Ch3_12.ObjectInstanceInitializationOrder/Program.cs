103c590b8499e2a62d430b6ef940731e95b254f6
saparkhid <parkhidpage@gmail.com>
2023-01-11 21:16:53 +0330

    feat: adds object instance initialization order example

---
+++ Ch3/Ch3_12.ObjectInstanceInitializationOrder/Ch3_12.ObjectInstanceInitializationOrder/Program.cs
@@ -0,0 +1,18 @@
+﻿public class A
+{
+    int x = 1; // Executes 3rd
+    public A(int x)
+    {
+        // Executes 4th
+    }
+}
+
+public class B : A
+{
+    int y = 1; // Executes 1st
+    public B(int x)
+    : base(x + 1) // Executes 2nd
+    {
+        // Executes 5th
+    }
+}
