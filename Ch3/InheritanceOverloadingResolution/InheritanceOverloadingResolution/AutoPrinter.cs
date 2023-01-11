0acd12cb0201c4b6dcae69b28e83a1bba4339880
saparkhid <parkhidpage@gmail.com>
2023-01-11 21:46:06 +0330

    feat: adds a projec about overloading selection

---
+++ Ch3/InheritanceOverloadingResolution/InheritanceOverloadingResolution/AutoPrinter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceOverloadingResolution
+{
+    public class AutoPrinter
+    {
+        public AutoPrinter()
+        {
+
+        }
+        public void Print(Car c)
+        {
+            Console.WriteLine("Car:" + c.Name);
+        }
+        public void Print(Vehicle v)
+        {
+            Console.WriteLine("Vehicle:" + v.Name);
+        }
+    }
+}
---
+++ Ch3/InheritanceOverloadingResolution/InheritanceOverloadingResolution/Program.cs
@@ -0,0 +1,8 @@
+﻿using InheritanceOverloadingResolution;
+AutoPrinter printer = new AutoPrinter();
+
+Car c1 = new Car("C1"); //compile time
+Vehicle v1 = new Car("VC2");//compile time
+printer.Print(c1);
+printer.Print(v1);
+printer.Print((dynamic)v1);//run time: outputs Car:VC2 , since v1 is a car at runtime
