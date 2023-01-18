dbf28a96ec4d3a2f27138d8b454ba80d39fd535b
saparkhid <parkhidpage@gmail.com>
2023-01-18 21:04:58 +0330

    feat: adds some code

--- Ch3/Ch3_15_GetType/Program.cs
+++ Ch3/Ch3_15_GetType/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,12 @@ namespace Ch3_15_GetType
     {
         static void Main(string[] args)
         {
+            int x = 10;
+            Console.WriteLine("Type of x at runtime:");
+            Console.WriteLine(x.GetType().ToString());
+
+            Console.WriteLine(x.GetType()== typeof(int));
+            Console.ReadKey();
         }
     }
 }
