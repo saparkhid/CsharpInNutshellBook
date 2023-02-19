0da7313b8d5ced7861fe1c3646653aff37a78eff
saparkhid <parkhidpage@gmail.com>
2023-02-19 21:50:15 +0330

    feat: adds an example to the generics constraints

---
+++ Ch3/Ch_23.GenericConstraintExample1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Ch_23.GenericConstraintExample1;
+
+T Max<T>(T a, T b) where T : IComparable<T>
+{
+   
+    if(a.CompareTo(b) > 0)
+    {
+        return a;
+    }
+    else if (a.CompareTo(b) < 0)
+    {
+        return b;
+    }
+    ///equal
+    return b;
+}
+
+int maxIntegers = Max(3, 5);
+Console.WriteLine(maxIntegers);
+Student st1 = new Student("Ali", 15);
+Student st2 = new Student("Reza", 18);
+Student maxStudent = Max(st1, st2);
+Console.WriteLine(maxStudent);
+Console.ReadKey();
---
+++ Ch3/Ch_23.GenericConstraintExample1/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_23.GenericConstraintExample1
+{
+    public class Student : IComparable<Student>
+    {
+        private string name;
+        private int age;
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public int CompareTo(Student? other)
+        {
+            if (this.age > other.age)
+            {
+                return 1;
+            }
+            else if (this.age < other.age)
+            {
+                return -1;
+            }
+            return 0;
+        }
+        public override string ToString()
+        {
+            return this.name + " is " + this.age.ToString() +" years old.";
+        }
+    }
+}
