84a15c5fee9401ca3cff1db0a4496170bb9dc6ea
saparkhid <parkhidpage@gmail.com>
2023-03-15 21:48:08 +0330

    delegate callback example

---
+++ Ch4/Ch_4.1.1_CalbackExample1/Downloader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_4._1._1_CalbackExample1
+{
+    public class Downloader
+    {
+        public delegate void DownloadFinishedCallback();
+        public void Download(string url, DownloadFinishedCallback callback)
+        {
+            Console.WriteLine("downloading file from {0}",url);
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("file downloaded successfully");
+            callback.Invoke();
+            // or simply    callback();
+        }
+    }
+}
---
+++ Ch4/Ch_4.1.1_CalbackExample1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Ch_4._1._1_CalbackExample1;
+
+Downloader downloader = new Downloader();
+
+downloader.Download("http://www.myfiles.co/data.txt", Email);
+
+downloader.Download("http://www.myfiles.co/data.txt", () =>
+{
+    Console.WriteLine("Shutting down the PC Now");
+});
+
+void Shutdown()
+{
+    Console.WriteLine("Shutting down the PC Now");
+}
+
+void Restart()
+{
+    Console.WriteLine("Restarting  the PC Now");
+}
+
+void Email()
+{
+    Console.WriteLine("Notify the user by sending an email");
+}
+
+Console.ReadKey();
